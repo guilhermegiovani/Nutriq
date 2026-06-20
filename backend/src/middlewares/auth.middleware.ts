54a764a1391af977e43ed2394f3bac043b87d77c
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2026-06-20 11:43:16 -0300

    feat(auth): add JWT authentication middleware

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import type { Request, Response, NextFunction } from 'express';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    // Implement your authentication logic here
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const [type, token] = authorizationHeader.split(' ');
+
+    if (type !== 'Bearer') {
+        return res.status(401).json({ message: 'Invalid token type' });
+    }
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token missing' });
+    }
+
+    try {
+        // Validate the token (this is just a placeholder, implement your actual validation logic)
+        const verifiedToken = jwt.verify(token, process.env.JWT_SECRET!);
+        const payload = verifiedToken as JwtPayload & {
+            userId: number;
+        };
+        
+        req.userId = payload.userId; // Assuming the token contains a user ID
+        console.log('Token verified:', verifiedToken);
+    } catch (error) {
+        return res.status(401).json({ message: 'Token validation failed' });
+    }
+
+    next();
+}
--- backend/src/routes/meals.route.ts
+++ backend/src/routes/meals.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getMealsController, createMealController, deleteMealController, updateMealController, getMealsByIdController } from '../controllers/meals.controller.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/', getMealsController);
+router.get('/', authMiddleware, getMealsController);
 router.get('/:id', getMealsByIdController);
 router.post('/', createMealController);
 router.delete('/:id', deleteMealController);
